34050f0b9812fe8e64c8f96ac014ff44043decde
Alex Adams <alex.adams@hudsonmx.com>
2021-12-06 19:18:39 -0500

    Add part 2

--- day1/src/main/java/DepthCounter.java
+++ day1/src/main/java/DepthCounter.java
@@ -1,5 +1,6 @@
 
 import common.InputUtil;
+import java.util.List;
 import java.util.stream.Collectors;
 import lombok.SneakyThrows;
 
@@ -9,18 +10,34 @@ public class DepthCounter {
 
     public static void main(String[] args)
     {
+        var elems = InputUtil.readInput(filename).stream().map(Integer::valueOf).collect(Collectors.toList());
         System.out.println("How many measurements are larger than the previous measurement?");
-        System.out.println("Answer: " + countLargerMeasurements(filename));
+        System.out.println("Answer: " + countLargerMeasurements(elems));
+        System.out.println("Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?");
+        System.out.println("Answer: " + countThreeMeasurementWindows(elems));
     }
 
     @SneakyThrows
-    private static int countLargerMeasurements(String filename){
-        var elems = InputUtil.readInput(filename).stream().map(Integer::valueOf).collect(Collectors.toList());
+    private static int countLargerMeasurements(List<Integer> elems){
         var count = 0;
         for (int i = 0; i < elems.size()-1; i++){
             if (elems.get(i+1) > elems.get(i)){
                 count++;
             }
+        }
+        return count;
+    }
+
+    @SneakyThrows
+    private static int countThreeMeasurementWindows(List<Integer> elems){
+        var count = 0;
+        var previousSum = 0;
+        for (int i = 0; i < elems.size()-3; i++){
+            var currentSum = elems.subList(i, i+3).stream().mapToInt(Integer::valueOf).sum();
+            if (currentSum > previousSum){
+                count++;
+            }
+            previousSum = currentSum;
         }
         return count;
     }
